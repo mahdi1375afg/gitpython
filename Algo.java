ca712083b721f1a96cec4189ecc1f517e2d7f549
mahdiafg <154007110+mahdi1375afg@users.noreply.github.com>
2024-03-20 20:54:42 +0100

    Update Algo.java
    59 eine line

--- Algo.java
+++ Algo.java
@@ -55,8 +55,7 @@ public static void main(String[] args) {
 	RestrictedCounter c = new RestrictedCounter(15);
 	for (int i=0; i<20; ++i) {
 	c.increment();
-	System.out.println("c=" + c.get() + " (free capacity=" +
-	c.freeCapacity() + ")" );
+	System.out.println("c=" + c.get() + " (free capacity=" +c.freeCapacity() + ")" );
 	}
 	for (int i=0; i<20; ++i) {
 	c.decrement();
